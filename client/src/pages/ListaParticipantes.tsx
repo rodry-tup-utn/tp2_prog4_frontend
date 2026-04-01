62a05fc9cb98205df324d2bad25f2c31a628f9e5
rodry <rodrymza@gmail.com>
2026-04-01 17:39:06 -0300

    agregada /node_modules a gitignore

--- client/src/pages/ListaParticipantes.tsx
+++ client/src/pages/ListaParticipantes.tsx
@@ -25,33 +25,37 @@ export const ListaParticipantes = () => {
       </div>
 
       <div className="overflow-x-auto">
-        <table className="w-full text-left border-collapse">
-          <thead className="bg-gray-50 uppercase text-xs font-semibold text-gray-500 border-b border-gray-200">
-            <tr>
-              <th className="px-6 py-4">Nombre</th>
-              <th className="px-6 py-4">Email</th>
-              <th className="px-6 py-4">Edad</th>
-              <th className="px-6 py-4">País</th>
-              <th className="px-6 py-4">Tecnologías</th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-100">
-            {participantes.length > 0 ? (
-              participantes.map((p) => (
-                <FilaParticipante key={p.id} participante={p} />
-              ))
-            ) : (
+        {vista === "tabla" && (
+          <table className="w-full text-left border-collapse">
+            <thead className="bg-gray-50 uppercase text-xs font-semibold text-gray-500 border-b border-gray-200">
               <tr>
-                <td
-                  colSpan={5}
-                  className="px-6 py-10 text-center text-gray-500 italic"
-                >
-                  No hay participantes registrados aún.
-                </td>
+                <th className="px-6 py-4">Nombre</th>
+                <th className="px-6 py-4">Email</th>
+                <th className="px-6 py-4">Edad</th>
+                <th className="px-6 py-4">País</th>
+                <th className="px-6 py-4">Tecnologías</th>
               </tr>
-            )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="divide-y divide-gray-100">
+              {participantes.length > 0 ? (
+                participantes.map((p) => (
+                  <FilaParticipante key={p.id} participante={p} />
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="px-6 py-10 text-center text-gray-500 italic"
+                  >
+                    No hay participantes registrados aún.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )}
+
+        {vista === " tarjetas" && ()}
       </div>
     </div>
   );

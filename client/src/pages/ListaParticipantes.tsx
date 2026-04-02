9cca4759d9b2576e8850ebfd6d0a9ebb7631db5c
rodry <rodrymza@gmail.com>
2026-04-02 00:42:37 -0300

    fix: cambio a vista sencilla sin tabla

--- client/src/pages/ListaParticipantes.tsx
+++ client/src/pages/ListaParticipantes.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { FilaParticipante } from "../components/FilaParticipante";
 import { useApi } from "../hooks/useApi";
 import { FiltroParticipantes } from "../components/FiltroParticipantes";
 import { useFiltros } from "../hooks/useFiltros";
@@ -15,7 +13,6 @@ export const ListaParticipantes = () => {
     opciones,
     errorOpciones,
   } = useApi();
-  const [vista, setVista] = useState<"tarjetas" | "tabla">("tarjetas");
   const { filtros, setFiltros, participantesFiltrados } =
     useFiltros(participantes);
 
@@ -42,31 +39,6 @@ export const ListaParticipantes = () => {
 
   return (
     <div className="bg-white shadow-xl rounded-2xl overflow-hidden border border-gray-100 p-3">
-      <div className="flex justify-end mb-6">
-        <div className="flex bg-gray-100 p-1 rounded-xl">
-          <button
-            onClick={() => setVista("tarjetas")}
-            className={`px-5 py-2 text-sm font-semibold rounded-lg transition-all duration-200 ${
-              vista === "tarjetas"
-                ? "bg-white text-blue-700 shadow-sm"
-                : "text-gray-500 hover:text-gray-800 hover:bg-gray-200/60"
-            }`}
-          >
-            Tarjetas
-          </button>
-
-          <button
-            onClick={() => setVista("tabla")}
-            className={`px-5 py-2 text-sm font-semibold rounded-lg transition-all duration-200 ${
-              vista === "tabla"
-                ? "bg-white text-blue-700 shadow-sm"
-                : "text-gray-500 hover:text-gray-800 hover:bg-gray-200/60"
-            }`}
-          >
-            Tabla
-          </button>
-        </div>
-      </div>
       <div className="p-6 bg-blue-900 rounded-xl flex flex-row items-center justify-around">
         <h2 className="text-xl font-bold text-white">Participantes</h2>
         <p className="p-3 rounded-2xl bg-blue-500 text-white font-bold">
@@ -83,46 +55,16 @@ export const ListaParticipantes = () => {
           opciones={opciones!}
           setFiltros={setFiltros}
         />
-        {vista === "tabla" && (
-          <table className="w-full text-left border-collapse">
-            <thead className="bg-gray-50 uppercase text-xs font-semibold text-gray-500 border-b border-gray-200">
-              <tr>
-                <th className="px-6 py-4">Nombre</th>
-                <th className="px-6 py-4">Email</th>
-                <th className="px-6 py-4">Edad</th>
-                <th className="px-6 py-4">País</th>
-                <th className="px-6 py-4">Tecnologías</th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-100">
-              {participantesFiltrados.length > 0 ? (
-                participantesFiltrados.map((p) => (
-                  <FilaParticipante key={p.id} participante={p} />
-                ))
-              ) : (
-                <tr>
-                  <td
-                    colSpan={5}
-                    className="px-6 py-10 text-center text-gray-500 italic"
-                  >
-                    No hay participantes registrados aún.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        )}
-        {vista == "tarjetas" && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {participantesFiltrados.map((participante) => (
-              <CardParticipante
-                key={participante.id || participante.nombre}
-                participante={participante}
-                handleEliminarParticipante={eliminarParticipante}
-              />
-            ))}
-          </div>
-        )}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {participantesFiltrados.map((participante) => (
+            <CardParticipante
+              key={participante.id || participante.nombre}
+              participante={participante}
+              handleEliminarParticipante={eliminarParticipante}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );

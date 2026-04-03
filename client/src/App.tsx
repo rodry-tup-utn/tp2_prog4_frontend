9486c109185dddeb68b5ddf7cac60b8ebb1cff7a
rodry <rodrymza@gmail.com>
2026-04-03 12:55:53 -0300

    configurado sonner en App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,11 @@ import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 import { FormularioRegistro } from "./pages/FormularioRegistro";
 import { ListaParticipantes } from "./pages/ListaParticipantes";
-
+import { Toaster } from "sonner";
 function App() {
   return (
     <>
+      <Toaster position="top-left" richColors></Toaster>
       <div className="min-h-screen flex flex-col bg-gray-50">
         <NavBar />
         <main className="grow w-full mx-auto px-6 py-8">

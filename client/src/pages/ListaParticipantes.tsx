fb7584eee61647398134c5b14a0270a629db6bc8
rodry <rodrymza@gmail.com>
2026-04-03 17:54:21 -0300

    feature:  uso de custom hooks, mejor visualizacion de errores, agregado toggle para modo 'offline'

--- client/src/pages/ListaParticipantes.tsx
+++ client/src/pages/ListaParticipantes.tsx
@@ -5,84 +5,99 @@ import { FiltroParticipantes } from "../components/FiltroParticipantes";
 import { useFiltros } from "../hooks/useFiltros";
 import { CardParticipante } from "../components/CardParticipante";
 import type { IUsuario } from "../types/usuario";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { toast } from "sonner";
+import { ToggleModo } from "../components/ToggleModo";
+import { MensajeError } from "../components/MensajeError";
+import { SpanTotal } from "../components/SpanTotal";
 
 export const ListaParticipantes = () => {
   const {
     participantes,
-    setParticipantes,
     loadingParticipantes,
     errorParticipantes,
     opciones,
     errorOpciones,
   } = useApi();
-  const [vista, setVista] = useState<"tarjetas" | "tabla">("tarjetas");
-  const { filtros, setFiltros, participantesFiltrados } =
-    useFiltros(participantes);
+  const { participantesLocal, quitarParticipanteLocal } = useLocalStorage();
+  const [vista, setVista] = useState("tarjetas");
+  const [modo, setModo] = useState("local");
 
-  //Eliminacion de participante en memoria, no en persistencia de base de datos, al refrescar pagina vuelve a aparecer
-  const eliminarParticipante = (participante: IUsuario) => {
-    const nuevalista = participantesFiltrados.filter(
-      (p) => p.id !== participante.id,
-    );
-    if (confirm(`Desea eliminar al usuario ${participante.nombre}?`))
-      setParticipantes(nuevalista);
+  const participantesUsados =
+    modo === "api" ? participantes : participantesLocal;
+
+  const { filtros, setFiltros, participantesFiltrados } =
+    useFiltros(participantesUsados);
+
+  const handleEliminarParticipante = (participante: IUsuario) => {
+    modo === "local"
+      ? quitarParticipanteLocal(participante)
+      : toast.warning(
+          "Funcion para borrar en base de datos próxima a desarrollar",
+        );
   };
 
   if (loadingParticipantes) {
     return <div className="text-center py-10">Cargando lista...</div>;
   }
-
-  if (errorParticipantes || errorOpciones) {
+  if (errorOpciones)
     return (
-      <div className="text-2xl bg-red-100 rounded-2xl text-center py-10 text-red-600">
-        Error al cargar participantes: {errorParticipantes}
-      </div>
+      <MensajeError
+        titulo="Error al cargar las opciones"
+        mensaje={errorOpciones}
+      />
     );
-  }
 
   return (
-    <div className="bg-white shadow-xl rounded-2xl overflow-hidden border border-gray-100 p-3">
-      <div className="flex justify-end mb-6">
-        <div className="flex bg-gray-100 p-1 rounded-xl">
-          <button
-            onClick={() => setVista("tarjetas")}
-            className={`px-5 py-2 text-sm font-semibold rounded-lg transition-all duration-200 ${
-              vista === "tarjetas"
-                ? "bg-white text-blue-700 shadow-sm"
-                : "text-gray-500 hover:text-gray-800 hover:bg-gray-200/60"
-            }`}
-          >
-            Tarjetas
-          </button>
+    <div className="bg-white shadow-xl rounded-2xl overflow-hidden border border-gray-100 p-3 max-w-7xl mx-auto">
+      <div className="flex justify-around mb-6">
+        <ToggleModo
+          titulo="Modo Offline"
+          modoActual={modo}
+          cambiarModo={setModo}
+          modoUno="api"
+          modoDos="local"
+          estiloActivo="bg-purple-700 text-white shadow-sm"
+          modoUnoLabel="Modo API"
+          modoDosLabel="Modo Local"
+        />
 
-          <button
-            onClick={() => setVista("tabla")}
-            className={`px-5 py-2 text-sm font-semibold rounded-lg transition-all duration-200 ${
-              vista === "tabla"
-                ? "bg-white text-blue-700 shadow-sm"
-                : "text-gray-500 hover:text-gray-800 hover:bg-gray-200/60"
-            }`}
-          >
-            Tabla
-          </button>
-        </div>
+        <ToggleModo
+          titulo="Cambiar Vista"
+          cambiarModo={setVista}
+          modoActual={vista}
+          estiloActivo="bg-teal-800 text-white shadow-sm"
+          modoUno="tabla"
+          modoUnoLabel="Tabla"
+          modoDos="tarjetas"
+          modoDosLabel="Tarjetas"
+        />
       </div>
-      <div className="p-6 bg-blue-900 rounded-xl flex flex-row items-center justify-around">
-        <h2 className="text-xl font-bold text-white">Participantes</h2>
-        <span className="p-3 rounded-2xl bg-blue-500 text-white font-bold">
-          Total registrados: {participantes.length}
-        </span>
-        <span className="p-3 rounded-2xl bg-blue-500 text-white font-bold">
-          Total mostrados: {participantesFiltrados.length}
-        </span>
+      <div className="bg-blue-900 rounded-2xl p-6 mb-6 flex flex-col md:flex-row items-center justify-around gap-4">
+        <h2 className="text-2xl font-bold text-white">Participantes</h2>
+
+        <div className="flex gap-3">
+          <SpanTotal
+            label="Total"
+            valor={participantesUsados.length}
+            color="blue"
+          />
+          <SpanTotal
+            label="Filtrados"
+            valor={participantesFiltrados.length}
+            color="teal"
+          />
+        </div>
       </div>
 
       <div className="overflow-x-auto">
-        <FiltroParticipantes
-          filtros={filtros}
-          opciones={opciones!}
-          setFiltros={setFiltros}
-        />
+        {opciones && (
+          <FiltroParticipantes
+            filtros={filtros}
+            opciones={opciones}
+            setFiltros={setFiltros}
+          />
+        )}
         {vista === "tabla" && (
           <table className="w-full text-left border-collapse">
             <thead className="bg-gray-50 uppercase text-xs font-semibold text-gray-500 border-b border-gray-200">
@@ -95,40 +110,48 @@ export const ListaParticipantes = () => {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-100">
-              {participantesFiltrados.length > 0 ? (
+              {participantesFiltrados.length > 0 &&
                 participantesFiltrados.map((p) => (
                   <FilaParticipante key={p.id} participante={p} />
-                ))
-              ) : (
-                <tr>
-                  <td
-                    colSpan={5}
-                    className="px-6 py-10 text-center text-gray-800 italic"
-                  >
-                    No se encontraron participantes
-                  </td>
-                </tr>
+                ))}
+              !
+              {!loadingParticipantes && !errorParticipantes && (
+                <div className="p-6 text-gray-600 italic text-center">
+                  No se encontraron participantes con esos filtros
+                </div>
               )}
             </tbody>
           </table>
         )}
         {vista == "tarjetas" && (
           <>
-            {participantesFiltrados.length > 0 ? (
+            {participantesFiltrados.length > 0 && (
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 {participantesFiltrados.map((participante) => (
                   <CardParticipante
-                    key={participante.id || participante.nombre}
+                    key={participante.id}
                     participante={participante}
-                    handleEliminarParticipante={eliminarParticipante}
+                    handleEliminarParticipante={handleEliminarParticipante}
                   />
                 ))}
               </div>
-            ) : (
+            )}
+            {!loadingParticipantes && !errorParticipantes && (
               <div className="p-6 text-gray-600 italic text-center">
-                No se encontraron participantes
+                No se encontraron participantes con esos filtros
               </div>
             )}
+            {errorParticipantes &&
+              (modo === "api" ? (
+                <MensajeError
+                  titulo="Error al conectarse con la API"
+                  mensaje={errorParticipantes}
+                />
+              ) : (
+                <div className="p-6 text-gray-600 italic text-center">
+                  No se encontraron participantes con esos filtros
+                </div>
+              ))}
           </>
         )}
       </div>

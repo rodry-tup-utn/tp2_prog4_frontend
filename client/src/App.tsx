052805c1314bafc6cef26cdd43e1fa172cd1641e
rodry <rodrymza@gmail.com>
2026-04-07 00:48:24 -0300

    feature: creado AppRouter para mejor organizacion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
-import { FormularioRegistro } from "./pages/FormularioRegistro";
-import { ListaParticipantes } from "./pages/ListaParticipantes";
 import { Toaster } from "sonner";
+import { AppRouter } from "./router/AppRouter";
+
 function App() {
   return (
     <>
@@ -11,13 +10,7 @@ function App() {
       <div className="min-h-screen flex flex-col bg-gray-50">
         <NavBar />
         <main className="grow w-full mx-auto px-6 py-8">
-          <Routes>
-            <Route path="/" element={<FormularioRegistro />} />
-
-            <Route path="/participantes" element={<ListaParticipantes />} />
-
-            <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
-          </Routes>
+          <AppRouter />
         </main>
         <Footer />
       </div>
---
+++ client/src/router/AppRouter.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { FormularioRegistro } from "../pages/FormularioRegistro";
+import { ListaParticipantes } from "../pages/ListaParticipantes";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<FormularioRegistro />} />
+
+      <Route path="/participantes" element={<ListaParticipantes />} />
+
+      <Route
+        path="*"
+        element={
+          <h2 className="text-red-700 p-6 rounded-2xl bg-red-100 text-center">
+            404 - Página no encontrada
+          </h2>
+        }
+      />
+    </Routes>
+  );
+};

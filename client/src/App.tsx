c2303c99dd24e04aae0c8ab189f6a4936a5e1480
rodry <rodrymza@gmail.com>
2026-04-13 13:05:15 -0300

    agregado ParticipanteProvider y BrowserRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,24 @@ import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 import { Toaster } from "sonner";
 import { AppRouter } from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
+import { ParticipantesProvider } from "./context/ParticipantesContext";
 
 function App() {
   return (
     <>
-      <Toaster position="top-left" richColors></Toaster>
-      <div className="min-h-screen flex flex-col bg-gray-50">
-        <NavBar />
-        <main className="grow w-full mx-auto px-6 py-8">
-          <AppRouter />
-        </main>
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <ParticipantesProvider>
+          <Toaster position="top-left" richColors></Toaster>
+          <div className="min-h-screen flex flex-col bg-gray-50">
+            <NavBar />
+            <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
+              <AppRouter />
+            </main>
+            <Footer />
+          </div>
+        </ParticipantesProvider>
+      </BrowserRouter>
     </>
   );
 }

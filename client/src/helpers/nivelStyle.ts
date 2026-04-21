3594c62b48632db02abd55f6bdf16bf4320337fc
rodry <rodrymza@gmail.com>
2026-04-21 16:20:38 -0300

    cambio de colores

--- client/src/helpers/nivelStyle.ts
+++ client/src/helpers/nivelStyle.ts
@@ -1,7 +1,7 @@
 export const getNivelColor = (nivel: string): string => {
-  if (nivel.toLowerCase() == "principiante") return "bg-green-400 text-white";
-  if (nivel.toLowerCase() == "intermedio") return "bg-orange-400 text-white";
-  if (nivel.toLowerCase() == "avanzado") return "bg-red-500 text-white";
+  if (nivel.toLowerCase() == "principiante") return "bg-green-600 text-white";
+  if (nivel.toLowerCase() == "intermedio") return "bg-orange-500 text-white";
+  if (nivel.toLowerCase() == "avanzado") return "bg-red-600 text-white";
 
   return "bg-grey-400";
 };

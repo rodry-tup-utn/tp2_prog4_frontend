790d8eda6ec3bbcbd1f1b39ea6dc2a303bfd5958
rodry <rodrymza@gmail.com>
2026-04-27 23:30:39 -0300

    feature: agregado provider de filtros

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,23 @@ import { Toaster } from "sonner";
 import { AppRouter } from "./router/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import { ParticipantesProvider } from "./context/ParticipantesContext";
+import { FiltrosProvider } from "./context/FiltrosContext";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <ParticipantesProvider>
-          <Toaster position="top-left" richColors></Toaster>
-          <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
-            <NavBar />
-            <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
-              <AppRouter />
-            </main>
-            <Footer />
-          </div>
+          <FiltrosProvider>
+            <Toaster position="top-left" richColors></Toaster>
+            <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
+              <NavBar />
+              <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
+                <AppRouter />
+              </main>
+              <Footer />
+            </div>
+          </FiltrosProvider>
         </ParticipantesProvider>
       </BrowserRouter>
     </>

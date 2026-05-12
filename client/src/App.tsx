1c808d116ff91f84f12b27ef8cce43b2472e3f07
rodry <rodrymza@gmail.com>
2026-05-11 22:04:23 -0300

    refactor: eliminacion de filtrosprovider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,23 +4,20 @@ import { Toaster } from "sonner";
 import { AppRouter } from "./router/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import { ParticipantesProvider } from "./context/ParticipantesContext";
-import { FiltrosProvider } from "./context/FiltrosContext";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <ParticipantesProvider>
-          <FiltrosProvider>
-            <Toaster position="top-left" richColors></Toaster>
-            <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
-              <NavBar />
-              <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
-                <AppRouter />
-              </main>
-              <Footer />
-            </div>
-          </FiltrosProvider>
+          <Toaster position="top-left" richColors></Toaster>
+          <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
+            <NavBar />
+            <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
+              <AppRouter />
+            </main>
+            <Footer />
+          </div>
         </ParticipantesProvider>
       </BrowserRouter>
     </>

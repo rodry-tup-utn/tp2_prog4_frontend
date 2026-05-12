13836c5877ee3d8ea77147a2e207e325ea8f3a0c
rodry <rodrymza@gmail.com>
2026-05-12 15:07:17 -0300

    refactor: cambiado provider de participantes por authprovider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,22 @@ import { Footer } from "./components/Footer";
 import { Toaster } from "sonner";
 import { AppRouter } from "./router/AppRouter";
 import { BrowserRouter } from "react-router-dom";
-import { ParticipantesProvider } from "./context/ParticipantesContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <ParticipantesProvider>
-          <Toaster position="top-left" richColors></Toaster>
-          <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
-            <NavBar />
-            <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
-              <AppRouter />
-            </main>
-            <Footer />
-          </div>
-        </ParticipantesProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Toaster position="top-left" richColors />
+        <div className="min-h-screen flex flex-col bg-linear-to-r from-blue-800 to-indigo-900">
+          <NavBar />
+          <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
+            <AppRouter />
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 

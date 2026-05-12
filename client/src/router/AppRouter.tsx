95ef3e20132566f5d83301ddc29a8e02f1baaaf5
rodry <rodrymza@gmail.com>
2026-05-12 15:11:33 -0300

    feat: rutas protegidas y publicas agregadas

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,15 +1,55 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import { FormularioRegistro } from "../pages/FormularioRegistro";
 import { ListaParticipantes } from "../pages/ListaParticipantes";
+import { LoginPage } from "../pages/LoginPage";
+import { Inicio } from "../pages/Inicio";
+import { RegisterUser } from "../pages/RegisterUser";
+import { PrivateRoute } from "../components/PrivateRoute";
+import { ParticipantesProvider } from "../context/ParticipantesContext";
+import { useAuth } from "../context/AuthContext";
+
+function PrivateLayout() {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  return (
+    <ParticipantesProvider>
+      <Outlet />
+    </ParticipantesProvider>
+  );
+}
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/participante/:id" element={<FormularioRegistro />}></Route>
-      <Route path="/participante" element={<FormularioRegistro />} />
-
-      <Route path="/" element={<ListaParticipantes />} />
-
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/inicio" element={<Inicio />} />
+      <Route element={<PrivateLayout />}>
+        <Route path="/" element={<ListaParticipantes />} />
+        <Route
+          path="/participante"
+          element={
+            <PrivateRoute requiredRole="ADMIN">
+              <FormularioRegistro />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/participante/:id"
+          element={
+            <PrivateRoute requiredRole="ADMIN">
+              <FormularioRegistro />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/usuarios/nuevo"
+          element={
+            <PrivateRoute requiredRole="ADMIN">
+              <RegisterUser />
+            </PrivateRoute>
+          }
+        />
+      </Route>
       <Route
         path="*"
         element={
